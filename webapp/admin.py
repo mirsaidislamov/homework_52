0e79af831e095a3558688e49a22566f8ef9e14bc
Mir-Said <mirsaid.islamov@gmail.com>
2025-10-27 16:46:25 +0600

    Создал модель задач и зарегестрировал её в админ панели

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from webapp.models import Tasks
 
-# Register your models here.
+@admin.register(Tasks)
+class TasksAdmin(admin.ModelAdmin):
+    list_display = ('id', 'description', 'status', 'date_to_complete')
+    list_filter = ('status', 'date_to_complete')
+    search_fields = ('description',)
+
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Tasks(models.Model):
+    status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
+
+    description = models.TextField(
+        verbose_name='Описание')
+    status = models.CharField(
+        max_length=20,
+        choices=status_choices,
+        default='new',
+        verbose_name='Статус'
+    )
+    date_to_complete = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name='Дата выполнения'
+    )
+
+    def __str__(self):
+        return f'{self.description} {self.status}'
