c2cda5d241774264d5ed02f5996d515e670abc33
Mir-Said <mirsaid.islamov@gmail.com>
2025-11-06 14:38:30 +0600

    Отредактировал task_create_view с учетом использования форм в проекте, добавил виджеты в форму и изменил шаблон для добавления задачи

--- webapp/forms.py
+++ webapp/forms.py
@@ -7,3 +7,9 @@ class TasksForm(forms.ModelForm):
     class Meta:
         model = Tasks
         fields = ['description', 'status', 'date_to_complete', 'detailed_description']
+        widgets = {
+            'description': forms.Textarea(attrs={'class': 'form-control', 'required': 'true'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+            'date_to_complete': forms.DateInput(attrs={'class': 'form-control'}),
+            'detailed_description': forms.Textarea(attrs={'class': 'form-control'}),
+        }
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Tasks, STATUS_CHOICES
-from datetime import datetime
+
+from webapp.models import Tasks
+from webapp.forms import TasksForm
+# from datetime import datetime
 
 
 def task_list_view(request):
@@ -12,38 +14,15 @@ def task_list_view(request):
 
 def task_create_view(request):
     if request.method == 'GET':
-        return render(request, 'task_add.html', {
-            'status_choices': STATUS_CHOICES
-        })
+        form = TasksForm()
+        return render(request, 'task_add.html', context={'form': form})
     elif request.method == 'POST':
-        description = request.POST.get('description')
-        status = request.POST.get('status')
-        date_to_complete_str = request.POST.get('date_to_complete')
-        detailed_description = request.POST.get('detailed_description')
-
-        if not description:
-            return render(request, 'task_add.html')
-
-        if status not in [s[0] for s in STATUS_CHOICES]:
-            status = 'new'
-
-        date_to_complete = None
-        if date_to_complete_str:
-            try:
-                date_to_complete = datetime.strptime(date_to_complete_str, '%Y-%m-%d').date()
-            except ValueError:
-                return render(request, 'task_add.html')
-
-        try:
-            task = Tasks.objects.create(
-                description=description,
-                status=status,
-                date_to_complete=date_to_complete,
-                detailed_description = detailed_description
-            )
-            return redirect('task_detail', pk=task.pk)
-        except Exception:
-            return redirect('task_create')
+        form = TasksForm(data=request.POST)
+        if form.is_valid():
+            task = form.save()
+            return redirect('task_detail',pk=task.id)
+        else:
+            return render(request, 'task_add.html', context={'form': form})
 
 
 def task_detail_view(request, *args, pk, **kwargs):
